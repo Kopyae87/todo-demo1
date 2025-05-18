3bb11f976ecfb3393af683c6c1d2ed9f23b0de7a
Kopyae87 <kopyae135385@gmail.com>
2025-05-18 09:52:49 +0630

    vue basic done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,58 @@
 <template>
   <v-container>
-    <h2>{{ title }}</h2>
+    
+    <v-row >
+      <v-col cols="12">
+        <h2>{{ title }}</h2>
+        <div>
+          <v-text-field label="Enter Name" v-model="name" @keydown.enter="changeflag" :disabled="flag"></v-text-field>
+            hello: {{ name }}
+        </div>
+      </v-col>
+    </v-row>
+    
+    <v-row>
+      <v-col cols="9">
+          <v-text-field label="What will you do" v-model="todo"></v-text-field>
+      </v-col>
+      <v-col cols="3">
+          <v-text-field label="How long it will take" v-model="duration"></v-text-field>
+      </v-col>
+    </v-row>
+    
     <div>
-          <v-text-field label="enter name" v-model="name"></v-text-field>
-            hello:{{ name }}
-            This is home page.
+      <v-btn @click="add">
+        Add To List
+      </v-btn>
     </div>
+    <div>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Id
+            </th>
+            <th class="text-left">
+              Todo
+            </th>
+            <th class="text-left">
+              Duration
+            </th>
 
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in todolist"
+            :key="item.id"
+          > 
+            <td>{{ item.id }}</td>
+            <td>{{ item.todo }}</td>
+            <td>{{ item.duration }} hr(s)</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
   </v-container>
 </template>
 
@@ -17,7 +63,29 @@ export default {
 
   data: () => ({
     title:"This is my first Vue Project.",
-    name:""
+    name:"",
+    flag:false,
+    todolist: [
+          {
+            id:1,
+            todo: 'Attend OJT',
+            duration: 3,
+          },
+          {
+            id:2,
+            todo: 'Clean the house',
+            duration: 0.5,
+          }
+      ]
   }),
+
+  methods:{
+    add(){
+      this.todolist.push({id:this.todolist.length+1, todo:this.todo , duration:this.duration})
+    },
+    changeflag(){
+      this.flag=!this.flag
+    }
+  }
 }
 </script>
